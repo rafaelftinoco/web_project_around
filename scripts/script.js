cdd2c938e08b6cc4d3c9da300a576fa3a20148bc
Rafael <rafaeltinoco68@gmail.com>
2024-07-31 14:23:58 -0300

    mudanças revisor

--- scripts/script.js
+++ scripts/script.js
@@ -13,24 +13,22 @@ addEventListener("click", function () {
   overlay.removeAttribute("overlay__hidden");
 });
 
-addEventListener("DOMContentLoaded", function () {
-  function openPopup() {
-    popup.style.display = "flex";
-    overlay.style.display = "block";
-  }
-  function closePopup() {
-    popup.style.display = "none";
-    overlay.style.display = "none";
-  }
-  editbtn.addEventListener("click", openPopup);
-  closepopupbtn.addEventListener("click", closePopup);
+function openPopup() {
+  popup.style.display = "flex";
+  overlay.style.display = "block";
+}
+function closePopup() {
+  popup.style.display = "none";
+  overlay.style.display = "none";
+}
+editbtn.addEventListener("click", openPopup);
+closepopupbtn.addEventListener("click", closePopup);
 
-  function editUserInfo(event) {
-    event.preventDefault();
-    profileName.textContent = inputname.value;
-    profileabtme.textContent = inputabtme.value;
-    closePopup();
-  }
+function editUserInfo(event) {
+  event.preventDefault();
+  profileName.textContent = inputname.value;
+  profileabtme.textContent = inputabtme.value;
+  closePopup();
+}
 
-  form.addEventListener("submit", editUserInfo);
-});
+form.addEventListener("submit", editUserInfo);
